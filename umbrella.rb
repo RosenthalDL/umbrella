2cfa67bbbb0b2419a67ec3375fbd2cac310d7636
Danielle Rosenthal <danieller530@gmail.com>
2025-01-24 15:14:58 +0000

    First commit

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,21 @@
+require "http"
+require "json"
+require "dotenv/load"
 # Write your soltuion here!
+
+#pp "What is your location?"
+
+#location = gets.chomp()
+
+#pp location
+
+#url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=" + ENV.fetch("GMAPS_KEY")
+url = "https://maps.googleapis.com/maps/api/geocode/json?address=Chicago&key=" + ENV.fetch("GMAPS_KEY")
+
+raw_response = HTTP.get(url)
+parsed_response = JSON.parse(raw_response)
+results = parsed_response.fetch("results")[0]
+geometry = results.fetch("geometry")
+location = geometry.fetch("location")
+latitude = location.fetch("lat")
+longitude = location.fetch("lng")
